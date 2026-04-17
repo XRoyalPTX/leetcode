b6b861935176a62c6b3aeb93f4d0c80d17f07cd3
vladislav <112802747+XRoyalPTX@users.noreply.github.com>
2026-04-17 18:40:03 +0300

    Delete 0001.py

--- 0001.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict_num = {}
-        for i in range(len(nums)):
-            if nums[i] in dict_num:
-                return [dict_num[nums[i]],i]
-            dict_num[target - nums[i]] = i
